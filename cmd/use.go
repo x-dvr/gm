7c0601e5654a9761906614e6506f15f87cbd8bc8
Denis Rodin <denis.rodin@proton.me>
2025-12-26 21:36:57 +0100

    feat: Implement use command

--- cmd/use.go
+++ cmd/use.go
@@ -4,9 +4,13 @@ Copyright © 2025 DENIS RODIN <denis.rodin@proton.me>
 package cmd
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/x-dvr/gm/sys"
+	"github.com/x-dvr/gm/toolchain"
 )
 
 // useCmd represents the use command
@@ -14,8 +18,21 @@ var useCmd = &cobra.Command{
 	Use:   "use",
 	Args:  cobra.ExactArgs(1),
 	Short: "Set specified version of Go toolchain as current",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("use called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		version := args[0]
+		if version == versionLatest {
+			version, err = toolchain.GetLatestVersion()
+			if err != nil {
+				slog.Error("Failed to get latest Go version", slog.String("error", err.Error()))
+				os.Exit(1)
+			}
+		}
+		if !strings.HasPrefix(version, "go") {
+			version = "go" + version
+		}
+
+		return sys.SetAsCurrent(version)
 	},
 }
 
