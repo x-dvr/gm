461beed0619106da41ecd4e6e8ab87346294c00d
Denis Rodin <denis.rodin@proton.me>
2025-12-26 21:11:35 +0100

    feat: Implement install command

--- cmd/install.go
+++ cmd/install.go
@@ -5,8 +5,13 @@ package cmd
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/x-dvr/gm/sys"
+	"github.com/x-dvr/gm/toolchain"
 )
 
 // installCmd represents the install command
@@ -17,7 +22,35 @@ var installCmd = &cobra.Command{
 	Short:   "Install specified version of Go toolchain",
 	Long:    fmt.Sprintf("Use '%s' to install most recent version of toolchain.", versionLatest),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("install called - %s\n", args[0])
+		var err error
+		version := args[0]
+		if version == versionLatest {
+			version, err = toolchain.GetLatestVersion()
+			if err != nil {
+				slog.Error("Failed to get latest Go version", slog.String("error", err.Error()))
+				os.Exit(1)
+			}
+		}
+		if !strings.HasPrefix(version, "go") {
+			version = "go" + version
+		}
+
+		destPath, err := sys.PathForVersion(version)
+		if err != nil {
+			slog.Error("Failed to determine	destination path for installation", slog.String("error", err.Error()))
+			os.Exit(1)
+		}
+
+		err = toolchain.Install(version, destPath)
+		if err != nil {
+			slog.Error("Failed to download toolchain", slog.String("error", err.Error()), slog.String("version", version), slog.String("path", destPath))
+			os.Exit(1)
+		}
+
+		if err := sys.SetAsCurrent(version); err != nil {
+			slog.Error("Failed to set installed toolchain as current", slog.String("error", err.Error()), slog.String("version", version))
+			os.Exit(1)
+		}
 	},
 }
 
---
+++ sys/sys.go
@@ -0,0 +1,89 @@
+/*
+Copyright © 2025 DENIS RODIN <denis.rodin@proton.me>
+*/
+package sys
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const (
+	gmDir     = ".gm"
+	workspace = "workspace"
+	versions  = "versions"
+	current   = "current"
+)
+
+var (
+	ErrNoPath       = errors.New("environment variable 'PATH' is not set")
+	ErrNotInstalled = errors.New("version is not installed")
+)
+
+func PathForVersion(version string) (string, error) {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("get home dir of user: %w", err)
+	}
+	return filepath.Join(homedir, gmDir, versions, version), nil
+}
+
+func SetAsCurrent(version string) error {
+	versionPath, err := PathForVersion(version)
+	if err != nil {
+		return fmt.Errorf("get path for version %q: %w", version, err)
+	}
+	currentPath, err := PathForVersion(current)
+	if err != nil {
+		return fmt.Errorf("get path for current version: %w", err)
+	}
+
+	if _, err := os.Stat(versionPath); err != nil {
+		if os.IsNotExist(err) {
+			return ErrNotInstalled
+		}
+		return fmt.Errorf("check installed version: %w", err)
+	}
+
+	if err := os.Remove(currentPath); err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("reset current version: %w", err)
+	}
+
+	return os.Symlink(versionPath, currentPath)
+}
+
+func SetGoEnvs() error {
+	path := os.Getenv("PATH")
+	if path == "" {
+		return ErrNoPath
+	}
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("get home dir of user: %w", err)
+	}
+
+	goPath := filepath.Join(homedir, gmDir, workspace)
+	goBin := filepath.Join(goPath, "bin")
+	goRoot := filepath.Join(homedir, gmDir, versions, current)
+	goSDKBin := filepath.Join(goRoot, "bin")
+
+	err = os.Setenv("GOPATH", goPath)
+	if err != nil {
+		return fmt.Errorf("set GOPATH: %w", err)
+	}
+	err = os.Setenv("GOBIN", goBin)
+	if err != nil {
+		return fmt.Errorf("set GOBIN: %w", err)
+	}
+	err = os.Setenv("GOROOT", goRoot)
+	if err != nil {
+		return fmt.Errorf("set GOROOT: %w", err)
+	}
+	err = os.Setenv("PATH", fmt.Sprintf("%s:%s:%s", path, goSDKBin, goBin))
+	if err != nil {
+		return fmt.Errorf("set PATH: %w", err)
+	}
+	return nil
+}
