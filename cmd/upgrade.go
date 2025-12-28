ba5e8eecd1a9f219f24e3abe6e4fcbfdadbd3414
Denis Rodin <denis.rodin@proton.me>
2025-12-28 13:37:55 +0100

    chore: Test version detect

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -5,8 +5,15 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	// set via -ldflags
+	version = "dev"
 )
 
 // upgradeCmd represents the upgrade command
@@ -16,8 +23,24 @@ var upgradeCmd = &cobra.Command{
 	Args:    cobra.ExactArgs(0),
 	Short:   "Upgrade self",
 	Long:    "Upgrade gm to latest version",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("upgrade called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		exePath, err := os.Executable()
+		if err != nil {
+			return fmt.Errorf("determine path of executable: %w", err)
+		}
+		fmt.Printf("upgrade called %s\n", exePath)
+
+		info, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Println("Build info not available")
+			return nil
+		}
+
+		fmt.Println("LD version:", version)
+		fmt.Println("Go version:", info.GoVersion)
+		fmt.Println("Module path:", info.Path)
+		fmt.Println("Main version:", info.Main.Version)
+		return nil
 	},
 }
 
